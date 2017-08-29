6679cf4c97914e04fb669137df9d0c50862b4f5d
Chopin <xiaoqinbang1993@qq.com>
2017-08-29 22:12:32 +0800

    1.解压字符串

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import string.UnZipString;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        UnZipString unZipString = new UnZipString();
+        System.out.println(unZipString.unZip("bb12{ efg }3{c3{ e4{ b2{tdds} }d }}ef"));
     }
 }
--- src/string/UnZipString.java
+++ src/string/UnZipString.java
@@ -1,4 +1,53 @@
 package string;
 
+import com.sun.deploy.util.StringUtils;
+
+/**
+ * 解压字符串
+ * 例如"bb12{efg}3{c3{e4{b2{tdds}}d}}ef"
+ */
 public class UnZipString {
+    public String unZip(String str) {
+        int headIndex, subHead = 0, endIndex = 0;
+        while (endIndex < str.length())
+        {
+            if (str.charAt(endIndex) == '{') { subHead = endIndex; }
+            if (str.charAt(endIndex) == '}')
+            {
+                while (subHead >= 0 && str.charAt(subHead) != '{') { subHead--; }
+                if (subHead < 0) {
+                    return "";
+                }
+                headIndex = subHead - 1;
+                int times = 0, base = 1;
+                while (headIndex >= 0 && isNum(str.charAt(headIndex)))
+                {
+                    times += base * (str.charAt(headIndex--) - '0');
+                    base *= 10;
+                }
+                if (times == 0) {
+                    return "";
+                }
+                headIndex++;
+                str = str.substring(0, headIndex) + timesStr(str.substring(subHead + 1, endIndex), times)
+                        + str.substring(endIndex + 1);
+                endIndex = headIndex + (endIndex - subHead - 1) * times;
+            }
+            else { endIndex++; }
+        }
+        return str;
+    }
+
+    private boolean isNum(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private String timesStr(String str, int times) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < times; i++)
+        {
+            result.append(str);
+        }
+        return result.substring(0);
+    }
 }
